f0ecb084907f4856724ef140455e2d51ddaa7026
Renato Paiva <paivarenato08@gmail.com>
2025-03-06 14:42:16 -0300

    Validando numero de cpf

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
-const camposDoFormulario = document.querySelectorAll("[required]");
+import ehUmCPF from "./valida-cpf.js";
+const camposDoFormulario = document.querySelectorAll('[required]')
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
 })
 
-function verificaCampo(campo){
-
+function verificaCampo(campo) {
+    if (campo.name == "cpf" && campo.value.length >= 11) {
+        ehUmCPF(campo);
+    }
 }
